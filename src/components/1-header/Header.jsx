559024f7d61660d9b80b2add427eca54bbb21753
hasanBafuleh <hasanbafuleh@gmail.com>
2024-03-10 09:22:42 +0300

    Creating the Nav bar

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,8 +1,32 @@
-import React from "react";
 import "./header.css";
 
 function Header() {
-  return <div>HEADER</div>;
+  return (
+    <header className="flex">
+      <div />
+      <nav>
+        <ul className="flex">
+          <li>
+            <a href="">About</a>
+          </li>
+
+          <li>
+            <a href="">Articles</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <button>light</button>
+    </header>
+  );
 }
 
 export default Header;
