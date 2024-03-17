dead5b56c02780947de015d669aa11070b98058b
hasanBafuleh <hasanbafuleh@gmail.com>
2024-03-17 23:47:29 +0300

    Starting with hero section

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./hero.css";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="hero flex">
+      <div className="left-section border">
+        <div className="main-avatar">
+          <img src="./personalPic.png" className="avatar" alt="" />
+          <span className=".icon-verified"></span>
+        </div>
+      </div>
+
+      <div className="right-section animation border">animation</div>
+    </section>
+  );
 }
 
 export default Hero;
