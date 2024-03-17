88c0f3957bbabf2c6d757e37fa48d184df4c4534
hasanBafuleh <hasanbafuleh@gmail.com>
2024-03-18 02:49:32 +0300

    Figuring out the content of the hero section

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -5,9 +5,18 @@ function Hero() {
   return (
     <section className="hero flex">
       <div className="left-section border">
-        <div className="main-avatar">
+        <div className="main-avatar flex">
           <img src="./personalPic.png" className="avatar" alt="" />
-          <span className=".icon-verified"></span>
+          <div className="icon-verified"></div>
+        </div>
+
+        <h1 className="title">Software Engineer, Full Stack</h1>
+        <p className="sub-title">I'm Hasan Bafuleh</p>
+
+        <div className="icons flex">
+          <div className="icon-twitter"></div>
+          <div className="icon-linkedin"></div>
+          <div className="icon-github"></div>
         </div>
       </div>
 
