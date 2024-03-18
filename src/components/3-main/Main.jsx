9a9242145fa42d5ee97ff9a3c3fb238ca82d67fc
hasanBafuleh <hasanbafuleh@gmail.com>
2024-03-19 02:09:13 +0300

    Adding the list in the main section

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./main.css";
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <main className="flex">
+      <section className="flex left-section">
+        <button className="active">All Projects</button>
+        <button>HTML & CSS</button>
+        <button>JavaScript</button>
+        <button>React & Next</button>
+        <button>Node & Express</button>
+      </section>
+
+      <section className="border right-section">rrrrrrrrr</section>
+    </main>
+  );
 }
 
 export default Main;
