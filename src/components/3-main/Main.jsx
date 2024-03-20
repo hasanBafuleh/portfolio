dabb05bdd795214f6ef8df3b9b3b665dfa95a0a0
hasanBafuleh <hasanbafuleh@gmail.com>
2024-03-20 23:40:07 +0300

    beginning with the first card

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -12,7 +12,15 @@ function Main() {
         <button>Node & Express</button>
       </section>
 
-      <section className="border right-section">rrrrrrrrr</section>
+      <section className="border right-section">
+        <article className="card">
+          <img src="" alt="" />
+          <div className="box">
+            <h1 className="title"></h1>
+            <p className="sub-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nihil officiis minus repudiandae eius maxime laborum, ex tempora similique nam nobis veniam pariatur</p>
+          </div>
+        </article>
+      </section>
     </main>
   );
 }
