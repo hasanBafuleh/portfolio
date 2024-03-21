ba736722a18f10b6558a8e7e9668cf5b2235ed7d
hasanBafuleh <hasanbafuleh@gmail.com>
2024-03-21 23:21:12 +0300

    Adding some Icons

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -12,12 +12,21 @@ function Main() {
         <button>Node & Express</button>
       </section>
 
-      <section className="border right-section">
-        <article className="card">
+      <section className="right-section">
+
+        <article className="border card">
           <img src="" alt="" />
           <div className="box">
             <h1 className="title"></h1>
             <p className="sub-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nihil officiis minus repudiandae eius maxime laborum, ex tempora similique nam nobis veniam pariatur</p>
+            <div className="flex">
+              <div className="icon-link"></div>
+              <div className="icon-github"></div>
+              <a href="">
+                more
+                <span className="icon-arrow-right"></span>
+              </a>
+            </div>
           </div>
         </article>
       </section>
