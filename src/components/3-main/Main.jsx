82336324e4810091405261e41ea349f3fae30794
hasanBafuleh <hasanbafuleh@gmail.com>
2024-03-25 06:07:27 +0300

    Make it responsive

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -12,28 +12,40 @@ function Main() {
         <button>Node & Express</button>
       </section>
 
-      <section className="right-section">
-        <article className="card">
-          <img width={270} style={{ padding: '0.4rem' }} src="./Promptopia.png" alt="" />
-          <div style={{ width: "270px" }} className=" box">
-            <h1 className="title">Propmtopia</h1>
-            <p className="sub-title">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              nihil officiis minus repudiandae eius maxime laborum, ex tempora
-              similique nam nobis veniam pariatur
-            </p>
-            <div className="flex icons">
-              <div style={{gap: "0.5rem"}} className="flex source">
-                <div className="icon-link"></div>
-                <div className="icon-github"></div>
+      <section className="flex right-section">
+        {["aa", "bb", "cc", 2, 3].map((item) => {
+          return (
+            <article key={item} className="card">
+              <img
+                width={270}
+                style={{ padding: "0.2rem" }}
+                src="./Promptopia.png"
+                alt=""
+              />
+              <div style={{ width: "270px" }} className=" box">
+                <h1 className="title">Propmtopia</h1>
+                <p className="sub-title">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Libero nihil officiis minus repudiandae eius maxime laborum,
+                  ex tempora similique nam nobis veniam pariatur
+                </p>
+                <div className="flex icons">
+                  <div style={{ gap: "0.5rem" }} className="flex source">
+                    <div className="icon-link"></div>
+                    <div className="icon-github"></div>
+                  </div>
+                  <a className="link flex" href="">
+                    more
+                    <span
+                      style={{ alignSelf: "end" }}
+                      className="icon-arrow-right"
+                    ></span>
+                  </a>
+                </div>
               </div>
-              <a className="link flex" href="">
-                more
-                <span style={{alignSelf: "end"}} className="icon-arrow-right"></span>
-              </a>
-            </div>
-          </div>
-        </article>
+            </article>
+          );
+        })}
       </section>
     </main>
   );
