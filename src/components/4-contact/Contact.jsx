dbc8666511175e4312b6080b5b6e9856e7466b67
hasanBafuleh <hasanbafuleh@gmail.com>
2024-03-27 08:53:07 +0300

    Completed the Contact Us left section

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import "./contact.css"
+import "./contact.css";
 
 function Contact() {
   return (
-    <section className='contact-us'>
-      
-      <h1>Contact us</h1>
+    <section className="contact-us">
+      <h1 className="title">
+        <span className="icon-envelope"></span>
+        Contact us
+      </h1>
+      <p className="sub-title">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere laborum
+        nulla corporis veritatis temporibus accusantium
+      </p>
+
+      <div className="flex">
+        <form className="">
+          <div className="flex" style={{ marginBottom: "24px" }}>
+            <label htmlFor="email">Email Address:</label>
+            <input required type="email" name="" id="email"></input>
+          </div>
+          <div className="flex">
+            <label htmlFor="message">Your message:</label>
+            <textarea required name="" id="message"></textarea>
+          </div>
+            <button className="submit">Submit</button>
+        </form>
+        <div className="animation border">Animation</div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
