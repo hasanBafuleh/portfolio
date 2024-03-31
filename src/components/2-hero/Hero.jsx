f8c1a9deea868f1f21ebbdaa110be2bbfedf357c
hasanBafuleh <hasanbafuleh@gmail.com>
2024-03-31 07:22:22 +0300

    Project first deployment

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 function Hero() {
   const titleText = "Software Engineer, Full Stack";
   const subText =
-    "I'm Hasan Bafuleh, a results-driven Software Engineer with an Associate Degree in Computer Science. My journey in software development has equipped me with a strong foundation in coding and efficient algorithms, making me well-prepared for new challenges in the tech industry. Eager to contribute to innovative projects, I actively engage in developing full-stack web applications using HTML, CSS, JavaScript, React, Node.js, Express.js, MongoDB, and Next.js. Furthermore, I possess experience in creating applications with Flutter..";
+    "I'm Hasan Bafuleh, a results-driven Software Engineer with a Degree in Computer Science. With a solid foundation in coding and efficient algorithms, I'm prepared for new challenges in tech. Eager to contribute to innovative projects, I actively engage in full-stack web development using HTML, CSS, JavaScript, React, Node.js, Express.js, MongoDB, and Next.js. Proficient in Flutter for application development.";
 
   return (
     <section className="hero flex">
@@ -53,7 +53,7 @@ function Hero() {
               key={i}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              transition={{ duration: 0.25, delay: i / 14 }}
+              transition={{ duration: 0.25, delay: i / 35 }}
             >
               {el}{" "}
             </motion.span>
@@ -71,10 +71,10 @@ function Hero() {
             <div className="icon icon-linkedin"></div>
           </a>
           <a
-          href="https://github.com/hasanBafuleh"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="all-icons"
+            href="https://github.com/hasanBafuleh"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="all-icons"
           >
             <div className="icon icon-github"></div>
           </a>
